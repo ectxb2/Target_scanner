ae4e4c8de142e7fd2ce7c52b47c62b9dd691fd4a
ectxb2 <trillere@msu.edu>
2023-02-24 10:27:33 -0500

    better comments

--- DBScan_tracks.py
+++ DBScan_tracks.py
@@ -8,30 +8,23 @@ from collections import Counter
 #ex:
 # python3 DBScan_tracks.py selftrigger_2022_08_05_00_04_09_PDT_evd.h5 3,6,8,78
 
-
-
-
-#Pass the fenction these things
+#Pass the function these things
 #Argument 1: name of selftrigger file, ex: selftrigger_2022_08_05_00_04_09_PDT_evd.h5
 #Argument 2: array of tracks to scan arround, but in specific format ex: 3,5,7,9
 
 
 detector_bounds = [[-150, 150], [-150, 150], [0, 300]] # mm (x, y, z)
-
 v_drift = 1.6 # mm/us (very rough estimate)
 clock_interval = 0.1 # us/tick -- 10 MHz clock rate
 drift_distance = detector_bounds[2][1] - detector_bounds[2][0] 
-
 drift_window = drift_distance/(v_drift*clock_interval) # maximum drift time
-
 drift_direction = 1 # +/- 1 depending on the direction of the drift in z
 
 #f = h5py.File('selftrigger_2022_08_05_05_06_01_PDT_evd.h5')
-#output_name=sys.argv[1] 
+
 f = h5py.File(sys.argv[1])
 
 eventData = f['charge']['events']['data']
-
 hitData = f['charge']['hits']['data']
 
 # this is a list of pairs of ints
@@ -112,23 +105,7 @@ def draw_labels(ax):
     plt.tight_layout()
     
 
-    
-event_num = 0
-t_num = 0
-#convert string from input to usable array
-t = sys.argv[2].split(',')
-t = [eval(i) for i in t]
 
-#plot tracks:
-
-#xseed = sys.argv[3].split(',')
-#xseed = [eval(i) for i in t]
-
-#yseed = sys.argv[4].split(',')
-#yseed = [eval(i) for i in t]
-
-#dist = 18.5 # pixel center to center diagonally asuming 4.4mm pixel pitch 
-dist = 25
 def make_colors(array):
     out = []
     for i in array:
@@ -150,11 +127,10 @@ def make_colors(array):
     return out
   
        
-
+#
 def draw_hits_dbscaned(event):
     """
-    Given a t0, plot all hits that would be within a 
-    drift window within the detector timeline
+    Make plot of DBScanned hits, color coded by cluster number
     """
     t0 = event['ts_start']
     tf = event['ts_end']
@@ -190,8 +166,10 @@ def draw_hits_dbscaned(event):
     
     return ax
 
-#center/average finding using weigted average
+
 def find_centers(db,px,py,q):
+    '''center/average finding of a cluster using weigted average from charge
+    '''
     labels = db.labels_
     n_clusters = len(set(labels)) - (1 if -1 in labels else 0) 
     x_centers = []
@@ -218,8 +196,17 @@ def find_centers(db,px,py,q):
         q_totals += [q_tot]
 
     return  x_centers,y_centers,q_totals
-     
-    
+
+
+event_num = 0
+t_num = 0
+
+#convert string from input to usable array
+t = sys.argv[2].split(',')
+t = [eval(i) for i in t]
+dist = 18.5 # pixel center to center diagonally asuming 4.4mm pixel pitch 
+
+#plot tracks:
     
 for event in eventData:
     if event_num == t[t_num]:
